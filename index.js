763edda7ff45b8cc27dcdb1934ab68ae708657b9
dovid yarmush <yarmushda@gmail.com>
2023-05-01 20:59:50 -0400

    completed scope lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = 'bob' 
+function returnBob() {
+    return customerName;
+};
+
+function upperCaseCustomerName() {
+    return customerName = 'BOB'
+};
+
+var bestCustomer
+function setBestCustomer() {
+    return bestCustomer = `not bob`
+};
+
+function overwriteBestCustomer() {
+    return bestCustomer = `maybe bob`
+};
+
+const leastFavoriteCustomer = 'mike';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = returnBob();
+};
+
+
+console.log(returnBob())
